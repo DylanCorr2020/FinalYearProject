b9e4f73046d0a3b40d723b57aa2c83ef55d33f1b
Dylan Corr <dylancorr.g@gmail.com>
2022-12-16 16:33:38 +0000

    Get meals from firestore database and display them on the screen

--- myfoodapp/lib/homepage.dart
+++ myfoodapp/lib/homepage.dart
@@ -1,9 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
-
   @override
   State<HomePage> createState() => _HomePageState();
 }
@@ -11,23 +11,65 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final user = FirebaseAuth.instance.currentUser!;
 
+  final mealsCollection = FirebaseFirestore.instance.collection('meals');
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('Signed In as:' + user.email!),
-          MaterialButton(
-            onPressed: () {
-              FirebaseAuth.instance.signOut();
-            },
-            color: Colors.red,
-            child: Text('Sign out'),
-          )
+      backgroundColor: Colors.grey[300],
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text('Menu'),
+        actions: [
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: MaterialButton(
+              onPressed: () {
+                FirebaseAuth.instance.signOut();
+              },
+              color: Colors.deepOrangeAccent,
+              child: Text('Sign out'),
+            ),
+          ),
         ],
-      )),
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: mealsCollection.snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return CircularProgressIndicator();
+          }
+
+          if (snapshot.data == null) {
+            return Text('No data');
+          }
+
+          final meals = snapshot.data!.docs;
+          
+          return ListView.builder(
+            itemCount: meals.length,
+            itemBuilder: (context, index) {
+              final meal = meals[index];
+              final name = meal['name'];
+              final description = meal['description'];
+              final price = meal['Price'];
+
+              return ListTile(
+                title: Text(name),
+                subtitle: Text(description),
+                trailing: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                 children: [
+                 Text('Price: \$$price'),
+                 ]
+                ),
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
